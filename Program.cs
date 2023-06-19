65cf79d9befc0be5246930a1ba2af4315c2264c2
Artur <ivan_ivankov@mail.ru>
2023-06-19 19:09:03 +0300

    3 задача, готовые 3 задачи

--- Program.cs
+++ Program.cs
@@ -67,3 +67,27 @@ Console.WriteLine(Math.Round(distance, 2));
 
 //____________________________________________________________________________________________________________________________________________________________________
 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int num = InputNum("Введите число: ");
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}");
+    if (i < num)
+    {
+        Console.Write(", ");
+    }
+    else
+    {
+        Console.Write(".");
+    }
+}
